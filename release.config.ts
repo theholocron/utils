78c78f4798d7649e50e697170c30368ed9bfb7af
Newton <5769156+iamnewton@users.noreply.github.com>
2026-07-22 15:54:50 -0700

    chore: fix release.config.ts
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- release.config.ts
+++ release.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "@theholocron/semantic-release-config";
 
 export default defineConfig({
-	branches: ["main", { name: "alpha", prerelease: true }],
 	exec: {
 		prepareCmd:
 			"node -e \"const fs=require('fs'),v='${nextRelease.version}'; ['packages/array-utils','packages/date-time-utils','packages/env-utils','packages/location-utils','packages/misc-utils','packages/storage-utils','packages/string-utils','packages/uri-utils'].forEach(p=>{const f=p+'/package.json',j=JSON.parse(fs.readFileSync(f));j.version=v;fs.writeFileSync(f,JSON.stringify(j,null,2)+'\\n');});\"",
