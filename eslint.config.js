acc2d4364a837e716db5d1ba015b3f7e61dc1c5a
Newton <5769156+iamnewton@users.noreply.github.com>
2026-07-22 15:00:35 -0700

    chore: bump @theholocron configs to 7.5.0, fix eslint ignoring dist/
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,6 @@ import { typescript } from "@theholocron/eslint-config";
  * @see https://eslint.org/docs/latest/use/configure/
  * @type {import("eslint").Linter.Config}
  */
-const config = [...typescript()];
+const config = [{ ignores: ["**/dist/**"] }, ...typescript()];
 
 export default config;
