5ce6e8e677ca411200c583e9ca142f8a1b87e1a4
Newton <5769156+iamnewton@users.noreply.github.com>
2026-08-01 17:46:11 -0700

    chore: ignore docs/.astro/ generated directory
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -1,7 +1,11 @@
+import starlight from "@astrojs/starlight";
 import { defineConfig } from "@theholocron/astro-config";
+import { docsTheme } from "@theholocron/docs-theme";
 import utilsConfig from "@theholocron/utils-docs";
 
 export default defineConfig({
 	docs: utilsConfig,
 	importMetaUrl: import.meta.url,
+	starlight,
+	docsTheme,
 });
