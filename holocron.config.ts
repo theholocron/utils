c6ca5649a149ee490f606ab0fb7d3c7072e5f0e7
Newton <5769156+iamnewton@users.noreply.github.com>
2026-08-19 11:02:22 -0700

    chore: 🔧 add codecov and bundle size to required checks
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- holocron.config.ts
+++ holocron.config.ts
@@ -9,6 +9,16 @@ export default defineConfig({
 	repo: {
 		...repo,
 		protection: "strict",
+		requiredChecks: [
+			"audit / Audit the bundle size",
+			"audit / Knip",
+			"codecov/patch",
+			"codecov/project/array",
+			"codecov/project/misc",
+			"codecov/project/storage",
+			"codecov/project/string",
+			"codecov/project/uri",
+		],
 		teams: [{ slug: "gatekeepers", permission: "maintain" }],
 		topics: [
 			"array",
